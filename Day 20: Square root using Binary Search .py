4490848d81687f3fd32b1d1815e205f23667cda1
sankaraJ <12147274+sankaraJ@users.noreply.github.com>
2021-06-08 20:37:46 +0300

    Create Day 20: Square root using Binary Search .py

---
+++ Day 20: Square root using Binary Search .py
@@ -0,0 +1,18 @@
+#Inspired by https://leetcode.com/explore/learn/card/binary-search/125/template-i/950/
+def mySqrt(x):
+  if x < 2:
+    return x
+  left, right = 2, x // 2
+
+  while left <= right:
+    pivot = left + (right - left) // 2
+    num = pivot * pivot
+
+    if num > x:
+      right = pivot - 1
+    elif num < x:
+      left = pivot + 1
+    else:
+      return pivot
+
+  return right
